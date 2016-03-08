072618248a93becf3dc1bac720eef6d2673f119f
Dehivis Perez <dehivis.oliver@gmail.com>
2016-03-08 10:15:31 -0430

    hola

--- registro.py
+++ registro.py
@@ -6,6 +6,9 @@ opc='S'
 registro=[]
 
 while opc == 'S' or opc == 's':
+    """
+    hola
+    """
     cedula=raw_input('ingrese la cedula: ')
     if len(cedula) <> 8:
         cedula=raw_input('debe ingresar una cedula valida (de 8 caracteres): ')
@@ -13,11 +16,17 @@ while opc == 'S' or opc == 's':
     nombre=raw_input('ingrese nombres: ')
     apellido=raw_input('ingrese apellido: ')
     sexo=raw_input('ingrese el sexo: ')
+    """
+    hola
+    """
 
     registro.append( cedula+' '+nombre+' '+apellido+' '+sexo )
     import pdb
     #pdb.set_trace()
     #print 'la persona registrada es: '+str(cedula)+' '+nombre+' '+apellido+' '+sexo
+    """
+    hola
+    """
 
 
     opc=raw_input('Desea continuar utilizando el programa (S/N) :')
